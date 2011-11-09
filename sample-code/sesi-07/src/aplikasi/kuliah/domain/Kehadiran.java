b4fd2e7cd21138a488b20215e95f8efdc2f3ffac
Endy Muhardin <endy.muhardin@gmail.com>
2011-11-09 12:58:56 +0700

    implement relasi class kehadiran dengan class mahasiswa, pertemuan

--- sample-code/sesi-07/src/aplikasi/kuliah/domain/Kehadiran.java
+++ sample-code/sesi-07/src/aplikasi/kuliah/domain/Kehadiran.java
@@ -1,5 +1,12 @@
 package aplikasi.kuliah.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Kehadiran {
     private String status;
+    
+    private Mahasiswa mahasiswa;
+    private List<Pertemuan> daftarPertemuan = 
+        new ArrayList<Pertemuan>();
 }

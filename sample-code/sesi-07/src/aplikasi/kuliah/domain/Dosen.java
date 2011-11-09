4b1328e7c326744ed7b89e483e9536fc4bdf8322
Endy Muhardin <endy.muhardin@gmail.com>
2011-11-09 13:02:06 +0700

    implement relasi class Dosen dengan class batch, pertemuan

--- sample-code/sesi-07/src/aplikasi/kuliah/domain/Dosen.java
+++ sample-code/sesi-07/src/aplikasi/kuliah/domain/Dosen.java
@@ -1,8 +1,17 @@
 package aplikasi.kuliah.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Dosen {
     private String nama;
     private String email;
+    
+    private List<Pertemuan> daftarPertemuan = 
+        new ArrayList<Pertemuan>();
+    
+    private List<Batch> daftarBatch = 
+        new ArrayList<Batch>();
     
     public Double hitungJamHadir(){
         return 0d;

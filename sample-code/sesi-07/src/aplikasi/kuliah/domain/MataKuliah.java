96f4e76934d0d9f58b341505478efe3d6987178f
Endy Muhardin <endy.muhardin@gmail.com>
2011-11-09 12:59:49 +0700

    implement relasi class matakuliah dengan class batch

--- sample-code/sesi-07/src/aplikasi/kuliah/domain/MataKuliah.java
+++ sample-code/sesi-07/src/aplikasi/kuliah/domain/MataKuliah.java
@@ -1,6 +1,12 @@
 package aplikasi.kuliah.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class MataKuliah {
     private String nama;
     private Integer sks;
+    
+    private List<Batch> daftarBatch = 
+        new ArrayList<Batch>();
 }

a5d1f296f86e7b032e80dae30d2e69690294510b
Endy Muhardin <endy.muhardin@gmail.com>
2011-11-09 12:57:12 +0700

    relasi class mahasiswa dengan class kehadiran, nilai, dan batch

--- sample-code/sesi-07/src/aplikasi/kuliah/domain/Mahasiswa.java
+++ sample-code/sesi-07/src/aplikasi/kuliah/domain/Mahasiswa.java
@@ -1,9 +1,21 @@
 package aplikasi.kuliah.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Mahasiswa {
     private String npm;
     private String nama;
     private String email;
+    
+    private List<Kehadiran> daftarKehadiran = 
+        new ArrayList<Kehadiran>();
+        
+    private List<Batch> daftarBatch = 
+        new ArrayList<Batch>();
+    
+    private List<Nilai> daftarNilai = 
+        new ArrayList<Nilai>();
     
     public Double hitungIPK(){
         return 0d;

d036d9b3f28a211436e5e93560a14c9b7205027e
Endy Muhardin <endy.muhardin@gmail.com>
2011-11-11 14:34:46 +0700

    skenario : 5 pendaftar

--- sample-code/sesi-07/batch-02/src/aplikasi/psb/Main.java
+++ sample-code/sesi-07/batch-02/src/aplikasi/psb/Main.java
@@ -1,5 +1,14 @@
 package aplikasi.psb;
 
 public class Main {
-
+    public static void main(String[] xx){
+        // 5 pendaftar
+        Pendaftar p1 = new Pendaftar();
+        Pendaftar p2 = new Pendaftar();
+        Pendaftar p3 = new Pendaftar();
+        Pendaftar p4 = new Pendaftar();
+        Pendaftar p5 = new Pendaftar();
+        
+        
+    }
 }

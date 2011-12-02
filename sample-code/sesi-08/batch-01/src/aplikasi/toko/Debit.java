69ee5f53dea05fe9dc8b2aec3e29087f6a02f5ff
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-02 14:15:19 +0700

    implement abstract method

---
+++ sample-code/sesi-08/batch-01/src/aplikasi/toko/Debit.java
@@ -0,0 +1,11 @@
+package aplikasi.toko;
+
+public class Debit extends NonTunai {
+    public Boolean valid(){
+        Boolean pinBenar = true;
+        if(pinBenar){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ sample-code/sesi-08/batch-01/src/aplikasi/toko/Kredit.java
@@ -0,0 +1,11 @@
+package aplikasi.toko;
+
+public class Kredit extends NonTunai {
+    public Boolean valid(){
+        Boolean noKartuValid = false;
+        if(noKartuValid){
+            return true;
+        }
+        return false;
+    }
+}

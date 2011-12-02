ce8d2410f91c66ae0a3ad24339b455d08e90dc35
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-02 14:13:09 +0700

    abstract class tidak bisa diinstankan

--- sample-code/sesi-08/batch-01/src/aplikasi/toko/Main.java
+++ sample-code/sesi-08/batch-01/src/aplikasi/toko/Main.java
@@ -5,10 +5,14 @@ public class Main {
         Pembayaran p = new Pembayaran();
         Pembayaran px = p; // tipe data sama
 
-        Tunai t = new Tunai();
+        Tunai t = new Tunai(); // concrete class, bisa diinstankan
+
         // Tunai adalah subclass, memiliki tipe data superclass
         px = t; // tidak error
 
         //t = p; // error, karena pembayaran memiliki informasi < tunai
+
+        // NonTunai n = new NonTunai(); // abstract class, tidak bisa diinstankan
+
     }
 }

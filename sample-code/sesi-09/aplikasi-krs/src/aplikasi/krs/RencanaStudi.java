9eb4022d46007b4f64e9410281f9ceb3c3668a2b
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-14 13:57:00 +0700

    relasi antar class

--- sample-code/sesi-09/aplikasi-krs/src/aplikasi/krs/RencanaStudi.java
+++ sample-code/sesi-09/aplikasi-krs/src/aplikasi/krs/RencanaStudi.java
@@ -1,8 +1,14 @@
 package aplikasi.krs;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class RencanaStudi {
     private String npm;
     private String nama;
     private Double ipk;
     private Integer semester;
+
+    private List<RencanaStudiDetail> daftarRencanaStudiDetail
+        = new ArrayList<RencanaStudiDetail>();
 }
--- sample-code/sesi-09/aplikasi-krs/src/aplikasi/krs/RencanaStudiDetail.java
+++ sample-code/sesi-09/aplikasi-krs/src/aplikasi/krs/RencanaStudiDetail.java
@@ -2,4 +2,7 @@ package aplikasi.krs;
 
 public class RencanaStudiDetail {
     private StatusRencanaStudiDetail status = StatusRencanaStudiDetail.PROPOSED;
+    private RencanaStudi rencanaStudi;
+    private Matakuliah matakuliah;
+    private Dosen reviewer;
 }

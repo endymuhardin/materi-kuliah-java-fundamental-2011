668fb2fb9275f039320007dfe97ea33003d02791
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-21 12:35:25 +0700

    property, getter/setter

--- sample-code/sesi-10/src/aplikasi/mahasiswa/Mahasiswa.java
+++ sample-code/sesi-10/src/aplikasi/mahasiswa/Mahasiswa.java
@@ -1,5 +1,22 @@
 package aplikasi.mahasiswa;
 
 public class Mahasiswa {
+    private String npm;
+    private String nama;
 
+    public String getNpm(){
+        return npm;
+    }
+
+    public void setNpm(String n){
+        npm = n;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setNama(String n){
+        nama = n;
+    }
 }

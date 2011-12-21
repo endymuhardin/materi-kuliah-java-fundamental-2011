93958ea268254f80fd0826355260c4e6f7f9123e
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-21 14:41:38 +0700

    implement database csv

--- sample-code/sesi-10/src/aplikasi/mahasiswa/AplikasiMahasiswa.java
+++ sample-code/sesi-10/src/aplikasi/mahasiswa/AplikasiMahasiswa.java
@@ -1,5 +1,32 @@
 package aplikasi.mahasiswa;
 
-public class AplikasiMahasiswa {
+import java.util.List;
+import java.util.ArrayList;
 
+public class AplikasiMahasiswa {
+    public static void main(String[] xx){
+        Mahasiswa m1 = new Mahasiswa();
+        m1.setNpm("123");
+        m1.setNama("Endy");
+
+        Mahasiswa m2 = new Mahasiswa();
+        m2.setNpm("456");
+        m2.setNama("Adi");
+
+        List<Mahasiswa> daftarMahasiswa = new ArrayList<Mahasiswa>();
+        daftarMahasiswa.add(m1);
+        daftarMahasiswa.add(m2);
+
+        List<DatabaseMahasiswa> db = new ArrayList<DatabaseMahasiswa>();
+
+        // database csv
+        DatabaseMahasiswaCsv dbCsv = new DatabaseMahasiswaCsv("mhs.txt", true);
+        db.add(dbCsv);
+
+        for(DatabaseMahasiswa d : db){
+            for(Mahasiswa m : daftarMahasiswa){
+                d.simpan(m);
+            }
+        }
+    }
 }
--- sample-code/sesi-10/src/aplikasi/mahasiswa/DatabaseMahasiswaCsv.java
+++ sample-code/sesi-10/src/aplikasi/mahasiswa/DatabaseMahasiswaCsv.java
@@ -1,7 +1,42 @@
 package aplikasi.mahasiswa;
 
-public class DatabaseMahasiswaCsv implements DatabaseMahasiswa {
-    public void simpan(Mahasiswa m){
+import java.io.File;
+import java.io.FileWriter;
 
+public class DatabaseMahasiswaCsv implements DatabaseMahasiswa {
+    private Boolean pakaiHeader;
+
+    private File file;
+    private FileWriter writer;
+
+    public DatabaseMahasiswaCsv(String lokasi, Boolean header){
+        try {
+            pakaiHeader = header;
+
+            file = new File(lokasi);
+            writer = new FileWriter(file, true);
+
+            // untuk file kosong, tulis header bila perlu
+            if(pakaiHeader && !(file.length() > 0)){
+                writer.write("npm,nama\n");
+                writer.flush();
+            }
+        } catch (Exception err){
+            err.printStackTrace();
+        }
+    }
+
+    public void simpan(Mahasiswa m){
+        try {
+            String npm = m.getNpm() == null ? "" : m.getNpm();
+            String nama = m.getNama() == null ? "" : m.getNama();
+
+            String record = String.format("%s,%s%n", npm, nama);
+            writer.write(record);
+            writer.flush();
+
+        } catch (Exception err){
+            err.printStackTrace();
+        }
     }
 }
